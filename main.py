af56f3fe08112658bd12f80c7fe6ef01b7a3307a
nateshungry <126623504+nateshungry@users.noreply.github.com>
2023-03-16 23:54:08 +0000

    done

--- main.py
+++ main.py
@@ -2,7 +2,20 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    pass
+  email = input('Enter your email string ')
+  flag = True
+  if not email[0].isalpha():
+    flag = False
+  lenemail = len(email)
+  if lenemail <= 5 or lenemail >= 30:
+    flag = False
+  if email.find('@') == -1:
+    flag = False
+  else:
+    atidx = email.find('@')
+  if email[atidx+1:].find('.') == -1:
+    flag = False
+  print(flag)
 
 
 if __name__ == '__main__':
